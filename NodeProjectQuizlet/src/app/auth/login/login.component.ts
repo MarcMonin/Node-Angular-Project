bb43d43acd4836efb8aea4209b3cb28ec5238754
ariste974 <aristemathiot@gmail.com>
2024-12-10 17:16:52 +0100

    Enlevage les mots de passe

--- NodeProjectQuizlet/src/app/auth/login/login.component.ts
+++ NodeProjectQuizlet/src/app/auth/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent {
   loginError='';
   formControls: any;
 
-  constructor(private fb: FormBuilder, private router: Router, private http:HttpClient) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
@@ -31,28 +31,9 @@ export class LoginComponent {
   }
 
   onLogin() {
-    console.log(this.loginForm.value);
-    if (this.loginForm.valid) {
-      // Récupérer les valeurs du formulaire
-      const formData = this.loginForm.value;
-      console.log('Form Data:', formData);
-      const { email, password } = this.loginForm.value;
-      console.log('Form is valid, submitting...');
-      // Envoi de la requête HTTP à l'API pour vérifier l'email et le mot de passe
-      this.http.post('http://localhost:3000/login', { email, password }).subscribe(
-        (response: any) => {
-          // Si la réponse est réussie, rediriger vers la page d'accueil
-          console.log('Login successful:', response);
-          this.isLoggedIn = true;
-          this.router.navigate(['/home']);
-        },
-        (error) => {
-          // Afficher l'erreur en cas de connexion échouée
-          this.loginError = error.error.message || 'Login failed';
-        }
-      );
-    }
-
-
+    console.log('Accessing without authentication...');
+    this.isLoggedIn = true;
+    this.router.navigate(['/home']);
   }
+
 }
